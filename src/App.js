e5898d2a8d6aca1d4fbbe84a86904db7ed53f6c2
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-04-17 03:30:08 +0600

    added to many folder where there need component for project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About/About";
+import Blogs from "./Pages/Blogs/Blogs";
 import Home from "./Pages/Home/Home/Home";
+import Login from "./Pages/Login/Login";
 import NotFound from "./Pages/NotFound/NotFound";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Header from "./Pages/Shared/Header/Header";
-
+import RequireAuth from "./Pages/Shared/RequireAuth/RequireAuth";
+import SignUp from "./Pages/SignUp/SignUp";
 function App() {
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/signup"
+          element={
+            <RequireAuth>
+              <SignUp />
+            </RequireAuth>
+          }
+        />
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
---
+++ src/Pages/About/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./About.css";
+const About = () => {
+  return (
+    <div>
+      <h2>About componet</h2>
+    </div>
+  );
+};
+
+export default About;
