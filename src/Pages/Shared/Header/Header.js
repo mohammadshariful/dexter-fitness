ef86f4c9b7ca7739c93727f3b5c399ebe2db188e
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-04-17 14:06:29 +0600

    signup page firebase implement

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,14 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
 import logo from "../../../Assets/Images/logo.png";
+import auth from "../../../Firebase/Firebase.init";
 import CustomLink from "../CustomLink/CustomLink";
 const Header = () => {
+  const [user] = useAuthState(auth);
+
   return (
     <Navbar collapseOnSelect sticky="top" expand="lg" bg="dark" variant="dark">
       <Container>
@@ -14,7 +19,13 @@ const Header = () => {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto justify-content-center align-items-center">
             <CustomLink to="/blogs">Blogs</CustomLink>
-            <CustomLink to="/login">Login</CustomLink>
+            {user ? (
+              <CustomLink to="/login" onClick={() => signOut(auth)}>
+                Sign Out
+              </CustomLink>
+            ) : (
+              <CustomLink to="/login">SignIn</CustomLink>
+            )}
             <CustomLink to="/signup">Sign Up</CustomLink>
             <CustomLink to="/about">About</CustomLink>
           </Nav>
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Form } from "react-bootstrap";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { FaEnvelope, FaLock, FaUser } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import signUpImg from "../../Assets/Images/login-signup/siignup.jpg";
+import auth from "../../Firebase/Firebase.init";
+import useStateHandle from "../../Hooks/useStateHandle";
+import Loading from "../Shared/Loading/Loading";
 import SocialLogin from "../Shared/SocialLogin/SocialLogin";
 import "./SignUp.css";
 const SignUp = () => {
+  const navigate = useNavigate();
+  const {
+    name,
+    email,
+    password,
+    confirmPassword,
+    handleName,
+    handleEmail,
+    handlePassword,
+    handleConfirmPassword,
+  } = useStateHandle();
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, [user]);
+  if (loading) {
+    return <Loading />;
+  }
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    createUserWithEmailAndPassword(emailValue, passwordValue);
+  };
   return (
     <Container className="my-5">
       <div className="d-flex justify-content-between flex-column flex-md-row">
@@ -14,42 +46,66 @@ const SignUp = () => {
         </div>
         <div className="form-container">
           <h2 className="text-center primary-color pb-2">Create an Account</h2>
-          <Form>
+          <Form onSubmit={handleFormSubmit}>
             <Form.Group className="mb-3 form-group" controlId="formBasicName">
               <Form.Label>Your Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter Name" />
+              <Form.Control
+                onBlur={handleName}
+                type="text"
+                placeholder="Enter Name"
+                required
+              />
               <div className="icon">
                 <FaUser />
               </div>
             </Form.Group>
             <Form.Group className="mb-3 form-group" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                onBlur={handleEmail}
+                type="email"
+                placeholder="Enter email"
+                required
+              />
               <div className="icon">
                 <FaEnvelope />
               </div>
             </Form.Group>
-
+            {email?.error && <p className="error">{email.error}</p>}
             <Form.Group
               className="mb-3 form-group"
               controlId="formBasicPassword"
             >
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                onBlur={handlePassword}
+                type="password"
+                placeholder="Password"
+                required
+              />
               <div className="icon">
                 <FaLock />
               </div>
             </Form.Group>
+            {password?.error && <p className="error">{password.error}</p>}
             <Form.Group
               className="mb-3 form-group"
               controlId="formBasicConfrimPassword"
             >
               <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" placeholder="Confrim Password" />
+              <Form.Control
+                onBlur={handleConfirmPassword}
+                type="password"
+                placeholder="Confrim Password"
+                required
+              />
               <div className="icon">
                 <FaLock />
               </div>
             </Form.Group>
+            {confirmPassword?.error && (
+              <p className="error">{confirmPassword.error}</p>
+            )}
             <button className="sign-login-btn">Sign Up</button>
           </Form>
           <SocialLogin />
