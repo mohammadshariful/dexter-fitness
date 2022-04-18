c6b345d36976b605dde53544f635012e190e67ae
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-04-18 13:25:02 +0600

    update blogs component

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -12,7 +12,7 @@ const Blogs = () => {
     <Container className=" my-5 p-3">
       <article className="blog">
         <h4 className="fw-bolder mb-3">
-          Title :Difference between authorization and authentication
+          Title :Difference between authentication and authorization
         </h4>
         <div className="px-3">
           <img
@@ -20,7 +20,28 @@ const Blogs = () => {
             src={authenticationVsAuthoraization}
             alt=""
           />
-          <p className="text-start my-3">authoraization</p>
+          <div className="text-start my-3">
+            <h5>Authentication</h5>
+            <ul>
+              <li>Authentication verifies who the user is.</li>
+              <li>
+                Authentication is visible to and partially changeable by the
+                user.
+              </li>
+              <li>
+                Authentication is the first step of a good identity and access
+                management process.
+              </li>
+            </ul>
+            <h5>Authorization</h5>
+            <ul>
+              <li>
+                Authorization determines what resources a user can access.
+              </li>
+              <li>Authorization isn't visible to or changeable by the user.</li>
+              <li>Authorization always takes place after authentication.</li>
+            </ul>
+          </div>
         </div>
         <div className="d-flex align-items-center px-3">
           <img className="author-img" src={author} alt="" />
@@ -43,9 +64,26 @@ const Blogs = () => {
           Title : Why are you using firebase? What other options do you have to
           implement authentication?
         </h4>
+
         <div className="px-3">
           <img className="blog-img img-fluid" src={firebase} alt="" />
-          <p className="text-start my-3">implement of firebase</p>
+          <div className="text-start my-3">
+            <p>
+              Firebase is used for many purposes that can help your apps to
+              develop, grow and make it a quality app. There are so many option
+              authentication without firebase. as like
+            </p>
+            <ul>
+              <li>Back4app </li>
+              <li>Kinvey </li>
+              <li>Parse </li>
+              <li>Pubnub </li>
+              <li>Backendless </li>
+              <li>Heroku </li>
+              <li>Appwrite </li>
+              etc...
+            </ul>
+          </div>
         </div>
         <div className="d-flex align-items-center px-3">
           <img className="author-img" src={author} alt="" />
@@ -70,7 +108,29 @@ const Blogs = () => {
         </h4>
         <div className="px-3">
           <img className="blog-img img-fluid" src={firebaseServices} alt="" />
-          <p className="text-start my-3">firebase services</p>
+          <div className="text-start my-3">
+            <p>
+              Firebase is Backedn service platform by Google that provides.It
+              have to mayny functionalities and helps with the backend
+              development of Android,iOS,web.
+            </p>
+            <h5>There are top functionalites here...</h5>
+            <p>
+              <b>Firebase Database:</b> used to store users data ,user details,
+              and other metadata.
+            </p>
+            <p>
+              <b>Firebase Cloud Storage:</b> used to store user-generated
+              content like the profile picture,multimedia messages etc.
+            </p>
+            <p>
+              <b>Firebase Cloud Messaging:</b> used to send notification
+            </p>
+            <p>
+              <b>Firebase Remote Config:</b> used to perform A/B testing on the
+              go
+            </p>
+          </div>
         </div>
         <div className="d-flex align-items-center px-3">
           <img className="author-img" src={author} alt="" />
